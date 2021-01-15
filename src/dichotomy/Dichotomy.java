01295c9f35fe9d8f631284292110cdc3251191d5
lxing <liuxing003@sinosoft.com.cn>
2021-01-15 10:20:14 +0800

    #修改_增加了二分法中一种使用和1位运算得到奇偶相关数

--- src/dichotomy/Dichotomy.java
+++ src/dichotomy/Dichotomy.java
@@ -3,6 +3,13 @@ package dichotomy;
 /**
  * 二分法
  *
+ * 部分对于位运算的使用
+ * & 1 (和 1 进行与运算AND);奇数为 1，偶数为 0
+ * ^ 1 (和 1 进行异或运算XOR);奇数 -1，偶数 +1
+ * | 1 (和 1 进行或运算OR);奇数不变，偶数 +1
+ * ~ 是单目运算符，按位取反，不讨论
+ *
+ *
  * @Author lx
  * @Date 2021/1/7 9:15
  * @Version 1.0
@@ -38,5 +45,47 @@ public class Dichotomy {
             }
         }
         return -1;
+    }
+
+    /**
+     * 有序的整数数组，每个数字都出现两次
+     * 只有一个出现一次，找出这个数
+     *
+     * O(n) XOR 即可，但是没有使用到有序的条件
+     * 因为也没有目标值，所以此处的有序条件，只是作为：相等两数相邻
+     *
+     * 考虑 1：中间两个值相等或者不相等，加上各自情况下左右分别是奇数还是偶数
+     * 有两个变量，且在两数不相等的情况下，判断出在哪侧比较复杂
+     *
+     * 考虑 2：下标从 0 开始，成对出现的数，第一个是偶数下标，下一个是挨着它的奇数下标
+     * 某个偶数下标的下一个不等于本身，说明包含在左侧区间内
+     *
+     * @param nums
+     * @return
+     */
+    public static int singleNonDuplicate(int[] nums) {
+        if (nums == null || nums.length < 1) {
+            return 0;
+        }
+        /**
+         * 数组长度为奇数个
+         * 搜索偶数下标的值，和它后边的一个值是否相等
+         * 相等，说明单个的数在右侧区间
+         *
+         * 边界问题，因为是找到特定元素的下标，且在循环中没有 return，
+         * 所以 left < right 不带 = ，避免一直循环下去（left == right, right = mid）
+         * mid ^ 1 保证了 mid +/- 1 与 mid 是偶数在前的相邻两数
+         *
+         */
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left & right) + ((left ^ right) >> 1);
+            if (nums[mid] == nums[mid ^ 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
     }
 }
