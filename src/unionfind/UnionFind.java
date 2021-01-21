90e69057d8b8411f1ad40c14c4968a17f3ff9f35
lxing <liuxing003@sinosoft.com.cn>
2021-01-21 11:28:29 +0800

    #修改_并查集模板中增加路径压缩

--- src/unionfind/UnionFind.java
+++ src/unionfind/UnionFind.java
@@ -9,23 +9,39 @@ package unionfind;
  */
 public class UnionFind {
 
-    private int[] leader;
+    private int[] parent;
+
+    private int[] rank;
 
     public UnionFind(int size) {
-        this.leader = new int[size];
+        this.parent = new int[size];
+        this.rank = new int[size];
         for (int i = 0; i < size; ++i) {
-            leader[i] = i;
+            parent[i] = i;
+            rank[i] = i;
         }
     }
 
     public void union(int index1, int index2) {
-        leader[find(index2)] = find(index1);
+        int root1 = find(index1);
+        int root2 = find(index2);
+        if (root1 == root2) {
+            return;
+        }
+        if (rank[root1] == rank[root2]) {
+            parent[root1] = root2;
+            ++rank[root2];
+        } else if (rank[root1] < rank[root2]) {
+            parent[root1] = root2;
+        } else {
+            parent[root2] = root1;
+        }
     }
 
     public int find(int index) {
-        if (leader[index] != index) {
-            leader[index] = find(leader[index]);
+        if (parent[index] != index) {
+            parent[index] = find(parent[index]);
         }
-        return leader[index];
+        return parent[index];
     }
 }
